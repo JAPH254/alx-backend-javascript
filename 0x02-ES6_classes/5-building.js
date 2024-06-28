144e31c813abe8d650acde2d2b5a16d3577e556c
JAPH254 <109793045+JAPH254@users.noreply.github.com>
2024-06-28 20:02:53 +0300

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,37 +1,20 @@
-/* eslint-disable no-underscore-dangle */
-/* eslint-disable class-methods-use-this */
 export default class Building {
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new TypeError('Cannot construct Building instances directly');
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
     }
-    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
   }
 
-  set sqft(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('Square Feet must be a number');
-    }
-    this._sqft = sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+  set sqft(value) {
+    this._sqft = value;
   }
 }
-
-// Example of a subclass that extends Building and implements the evacuationWarningMessage method
-class OfficeBuilding extends Building {
-  evacuationWarningMessage() {
-    return 'Please evacuate the office building immediately!';
-  }
-}
-
-// Usage example
-const office = new OfficeBuilding(5000);
-console.log(office.sqft); // 5000
-console.log(office.evacuationWarningMessage()); // 'Please evacuate the office building immediately!'
