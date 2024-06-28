93d2278c0b480f718afc44c187097fb775ec3b7b
JAPH254 <109793045+JAPH254@users.noreply.github.com>
2024-06-28 20:34:06 +0300

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,25 +1,10 @@
+/* eslint-disable */
 export default function createIteratorObject(report) {
-  // Extract all employees from all departments
-  const allEmployees = Object.values(report.departments).flatMap(employees => employees);
-
-  // Initialize index for iteration
-  let index = 0;
-
-  // Return the iterator object
-  return {
-    // Symbol.iterator function makes the object iterable
-    [Symbol.iterator]: function() {
-      return {
-        // next() method returns the next employee in sequence
-        next: function() {
-          if (index < allEmployees.length) {
-            return { value: allEmployees[index++], done: false };
-          } else {
-            // Iteration is complete
-            return { done: true };
-          }
+   let employee = [];
+   for (let [depts, emplys] of Object.entries(report.allEmployees)){
+        for (let emp of emplys){
+            employee.push(emp);
         }
-      };
-    }
-  };
+   }
+   return employee;
 }
