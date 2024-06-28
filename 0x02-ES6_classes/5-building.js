58b7dd146ab2f76205fd8c8771a700a4c3a213cd
JAPH254 <109793045+JAPH254@users.noreply.github.com>
2024-06-28 19:49:08 +0300

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,6 +2,9 @@
 /* eslint-disable class-methods-use-this */
 export default class Building {
   constructor(sqft) {
+    if (new.target === Building) {
+      throw new TypeError('Cannot construct Building instances directly');
+    }
     this._sqft = sqft;
   }
 
@@ -10,9 +13,13 @@ export default class Building {
   }
 
   set sqft(sqft) {
-    if (typeof (sqft) !== 'number') {
+    if (typeof sqft !== 'number') {
       throw new TypeError('Square Feet must be a number');
     }
     this._sqft = sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
