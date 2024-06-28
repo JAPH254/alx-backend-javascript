3223cc33691f7bc4dff167fc666848cee7f7ff60
JAPH254 <109793045+JAPH254@users.noreply.github.com>
2024-06-28 20:28:44 +0300

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,22 +1,13 @@
-import Car from './10-car.js'; // Assuming 10-car.js is in the same directory
+import Car from './10-car';
 
-class EVCar extends Car {
+export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    super(brand, "Electric", color); // Set model to "Electric" for all EVCars
-    this._motor = motor;
+    super(brand, motor, color);
     this._range = range;
   }
 
-  get motor() {
-    return this._motor;
-  }
-
-  get range() {
-    return this._range;
-  }
-
-  // Override cloneCar to return a Car object, hiding EVCar-specific details
+  // eslint-disable-next-line
   cloneCar() {
-    return new Car(this._brand, this._model, this._color);
+    return new Car[Symbol.species]();
   }
 }
